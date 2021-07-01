144a2b74ecfe5c77a0f8b6fdef91884cd9db6e6d
Erol <veliev77@mail.ru>
2021-07-01 21:01:13 +0300

    added index.html src

--- src/app/init.js
+++ src/app/init.js
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Webpack Setup</title>
-
-    </head>
-    <body>
-    <h3>Simple Webpack Setup</h3>
-    <app>    
-        <scrypt>
-        const { scrypt } = require("crypto")
-
-            import './a.js';
-            import './b.js';
-            console.log('./init');
-        </scrypt></app>
-    </body>
-</html>
+import './a.js';
+import './b.js';
+console.log('./init');
