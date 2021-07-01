8b51f038923c956a85255db52dced41201f5c8fd
Erol <veliev77@mail.ru>
2021-07-01 20:04:37 +0300

    added webpack and blade loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, 'app', 'index','/app/init.js'),
+  entry: path.join(__dirname + '/src/app/init.js'),
   watch: true,
   output: {
     path: path.join(__dirname, 'dist'),
@@ -30,12 +30,17 @@ module.exports = {
       }
     }]
   },
+  plugins: [  // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+        template: __dirname + "/src/public/init.js",
+        inject: 'body'
+    }),
   resolve: {
     extensions: ['.json', '.js', '.jsx']
   },
   devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, '/dist/'),
+    contentBase: path.join(__dirname, './src/public'),
     inline: true,
     host: 'localhost',
     port: 8080,
