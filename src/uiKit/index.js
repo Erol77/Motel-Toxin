a40be720e00d8c5c7d905584b96221e89f34aa77
Erol <veliev77@mail.ru>
2021-07-22 13:35:07 +0300

    added button

--- src/uiKit/index.js
+++ src/uiKit/index.js
@@ -5,12 +5,14 @@ import "./scss/style.scss";
 // import "./script.js";
 // $('body').css({'background-color': 'green'});
 // $('h1#title').text('Hello Webpack');
-const heading = document.createElement('h1');
-heading.textContent = 'Как интересно!';
+
+// const heading = document.createElement('h1');
+// heading.textContent = 'Как интересно!';
 
 // добавляем заголовок в DOM
-const root = document.querySelector('#root');
-root.append(heading);
+// const root = document.querySelector('#root');
+// root.append(heading);
+
 // import "./index.pug";
 // pug.render('p Hello world!');
 // console.log("hello world!");
