f070620facc02a7ed51607c31a29f0e9c40e0f68
Erol <veliev77@mail.ru>
2021-10-28 13:15:49 +0300

    delete educational materials

--- src/app/init.js
+++
@@ -1,7 +0,0 @@
-  
-    import './a.js' ;  
-    import './post.js' ;  
-     import './b.js' ;  
-  //   console.log('./init', a.js) 
-  const post = new Post( 'Webpack Post Title');
-  console.log('Post to String' , post.toString());
